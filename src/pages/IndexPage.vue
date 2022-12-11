fab5f73948968d5a6f5a10797b8357c07dec2705
YadevJayachandran <yadev64@gmail.com>
2022-12-11 12:29:35 +0530

    Package and home page updates

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,33 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page>
+    <div class="" style="height: 100vh">
+      <q-img src="~assets/home_cover.png" fit="cover" style="height: 100%">
+        <div class="absolute-full text-subtitle2 flex flex-center home_bg">
+          <div class="row">
+            <div class="col-md-6 col-sm-12">
+              <h2>“The Unicorn is About to Take Off”</h2>
+              <p>
+                Come, dive into our world that’ll build you to embrace your
+                tech-dreams and to sprout your wings.
+              </p>
+              <q-btn color="purple">Join Now</q-btn>
+            </div>
+          </div>
+        </div>
+      </q-img>
+    </div>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'IndexPage'
-})
+  name: "IndexPage",
+});
 </script>
+<style scoped>
+.home_bg {
+  background-color: rgba(11, 14, 41, 0.72);
+}
+</style>
