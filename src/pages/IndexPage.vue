051804b141b408d7fddd537de3869046c8b33288
YadevJayachandran <yadev64@gmail.com>
2022-12-11 15:01:19 +0530

    Bottom Nav tabs added

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -10,14 +10,27 @@
                 Come, dive into our world that’ll build you to embrace your
                 tech-dreams and to sprout your wings.
               </h6>
-              <q-btn color="primary">Join Now</q-btn>
+              <q-btn color="primary"><b>Join Now</b></q-btn>
             </div>
+
+            <q-img
+              src="~assets/pattern.png"
+              fit="contain"
+              class="lt-md"
+              style="
+                max-width: 700px;
+                width: 100%;
+                max-height: 429.8px;
+                height: 100%;
+              "
+            ></q-img>
           </div>
         </div>
       </q-img>
       <q-img
         src="~assets/pattern.png"
         fit="contain"
+        class="gt-sm"
         style="
           position: absolute;
           bottom: 0;
@@ -27,15 +40,58 @@
           height: 429.8px;
         "
       ></q-img>
+
+      <div
+        class="row"
+        style="position: absolute; bottom: 0; left: 0; width: 100%"
+      >
+        <div class="col-md-6 gt-sm">
+          <q-tabs
+            v-model="tab"
+            inline-label
+            :breakpoint="0"
+            align="justify"
+            class="bg-transparent text-white"
+          >
+            <q-tab name="contact" label="Contact" />
+            <q-tab name="alarms" label="FAQs" />
+            <q-tab v-if="$q.screen.gt.sm" name="movies" label="Campus" />
+            <q-tab v-if="$q.screen.gt.sm" name="photos" label="Opportunities" />
+            <q-btn-dropdown
+              v-if="$q.screen.lt.md"
+              auto-close
+              stretch
+              flat
+              label="More..."
+            >
+              <q-list>
+                <q-item clickable @click="tab = 'movies'">
+                  <q-item-section>Movies</q-item-section>
+                </q-item>
+
+                <q-item clickable @click="tab = 'photos'">
+                  <q-item-section>Photos</q-item-section>
+                </q-item>
+              </q-list>
+            </q-btn-dropdown>
+          </q-tabs>
+        </div>
+      </div>
     </div>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 
 export default defineComponent({
   name: "IndexPage",
+
+  setup() {
+    return {
+      tab: ref("mails"),
+    };
+  },
 });
 </script>
 <style scoped>
