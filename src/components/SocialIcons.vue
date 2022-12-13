2205f921d4444f7d1c858646c91196da7e9948c6
YadevJayachandran <yadev64@gmail.com>
2022-12-13 11:26:49 +0530

    Social icons component

---
+++ src/components/SocialIcons.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="row q-py-md">
+    <q-icon class="q-pr-md iconstyle" name="ion-logo-facebook"></q-icon>
+    <q-icon class="q-pr-md iconstyle" name="ion-logo-instagram"></q-icon>
+    <q-icon class="q-pr-md iconstyle" name="ion-logo-linkedin"></q-icon>
+    <q-icon class="q-pr-md iconstyle" name="ion-logo-twitter"></q-icon>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+<style scoped>
+.iconstyle {
+  color: white;
+  font-size: 30px;
+}
+:hover {
+  color: rgb(0, 255, 81);
+  font-size: 32px;
+}
+</style>
