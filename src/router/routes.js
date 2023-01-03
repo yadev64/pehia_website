560c513d2433f4e84b6f3bfda7088d18bc48ab00
YadevJayachandran <yadev64@gmail.com>
2023-01-03 22:04:56 +0530

    Routes updates

--- src/router/routes.js
+++ src/router/routes.js
@@ -9,6 +9,25 @@ const routes = [
     component: () => import("layouts/MainLayout.vue"),
     children: [{ path: "", component: () => import("pages/SummitPage.vue") }],
   },
+  {
+    path: "/summit2020",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [
+      { path: "", component: () => import("pages/SummitPage2020.vue") },
+    ],
+  },
+  {
+    path: "/summit2021",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [
+      { path: "", component: () => import("pages/SummitPage2021.vue") },
+    ],
+  },
+  {
+    path: "/about",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [{ path: "", component: () => import("pages/AboutPage.vue") }],
+  },
 
   // Always leave this as last one,
   // but you can also remove it
