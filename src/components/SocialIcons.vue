29f050e81a236f81d4578248872d3f6939bd4940
YadevJayachandran <yadev64@gmail.com>
2023-01-03 22:03:39 +0530

    Social icons updated content

--- src/components/SocialIcons.vue
+++ src/components/SocialIcons.vue
@@ -1,9 +1,37 @@
 <template>
   <div class="row q-py-md">
-    <q-icon class="q-pr-md iconstyle" name="ion-logo-facebook"></q-icon>
-    <q-icon class="q-pr-md iconstyle" name="ion-logo-instagram"></q-icon>
-    <q-icon class="q-pr-md iconstyle" name="ion-logo-linkedin"></q-icon>
-    <q-icon class="q-pr-md iconstyle" name="ion-logo-twitter"></q-icon>
+    <a
+      href="https://www.facebook.com/pehiaorg/"
+      target="_blank"
+      style="text-decoration: none"
+      class="text-white"
+    >
+      <q-icon class="q-pr-md iconstyle" name="ion-logo-facebook"></q-icon>
+    </a>
+    <a
+      href="https://www.instagram.com/pehiaorg/"
+      target="_blank"
+      style="text-decoration: none"
+      class="text-white"
+    >
+      <q-icon class="q-pr-md iconstyle" name="ion-logo-instagram"></q-icon>
+    </a>
+    <a
+      href="https://www.linkedin.com/company/pehia-foundation/"
+      target="_blank"
+      style="text-decoration: none"
+      class="text-white"
+    >
+      <q-icon class="q-pr-md iconstyle" name="ion-logo-linkedin"></q-icon>
+    </a>
+    <a
+      href="https://twitter.com/pehiaorg"
+      target="_blank"
+      style="text-decoration: none"
+      class="text-white"
+    >
+      <q-icon class="q-pr-md iconstyle" name="ion-logo-twitter"></q-icon>
+    </a>
   </div>
 </template>
 
