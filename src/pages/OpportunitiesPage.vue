18c3588110f4e07e27c3335f12620a5b65ce0439
YadevJayachandran <yadev64@gmail.com>
2023-01-06 11:20:45 +0530

    Opportunities page created

---
+++ src/pages/OpportunitiesPage.vue
@@ -0,0 +1,297 @@
+<template>
+  <q-page class="bg-secondary" padding>
+    <div class="row">
+      <div class="col-md-12 col-sm-12">
+        <div class="q-pa-lg text-white">
+          <h2>Opportunities from Pehia</h2>
+          <p style="font-size: 26px">
+            Pehia has always aimed at providing the best assistance to anyone
+            who wanted to excel in the world of technology, and is keenly
+            working towards bridging the gender gap in technology. As part of
+            it, Pehia offers some brilliant internships and job opportunities.
+          </p>
+          <h3>Pehia Fantacode Fellowship</h3>
+          <div class="row">
+            <div class="col-md-6 col-sm-12 q-pa-md">
+              <h4>Introduction</h4>
+              <p style="font-size: 26px">
+                Pehia Foundation together with Fantacode Solutions LLP is
+                looking for a skilled, motivated, girl coder aspirants to be
+                part of their fellowship program. The fellowship will be of a
+                kind of technical internship from Fantacode with some holistic
+                development power coursed by Pehia Foundation. During the
+                internship, the interns will have to complete some specific
+                tasks and responsibilities to develop their capabilities and
+                gain experience.
+              </p>
+            </div>
+            <div class="col-md-6 col-sm-12 q-pa-md">
+              <h4>Objective</h4>
+              <p style="font-size: 26px">
+                The goal of the program is to empower four amazing women out of
+                being code aspirants. They will act as role models for women in
+                tech. On completion of the program, interns will attain the
+                confidence to work on the real project and see themselves
+                speaking at conferences or other events which they once used to
+                be an attendee.
+              </p>
+            </div>
+          </div>
+
+          <h4>Program Design</h4>
+
+          <div class="row">
+            <div class="col-md-6 col-sm-12 q-pa-md">
+              <p style="font-size: 26px"><b>Requirements:</b>></p>
+              <p style="font-size: 26px">
+                The team is looking for the girls who have completed their under
+                graduation course. Those who are hardworking and have a strong
+                desire to learn can apply. The applicants are expected to be
+                proficient in basic programming skills and problem-solving
+                skills.
+              </p>
+            </div>
+            <div class="col-md-6 col-sm-12 q-pa-md">
+              <p style="font-size: 26px">
+                <b>Stipend:</b>
+              </p>
+              <p style="font-size: 26px">
+                This is a voluntary basis internship and no stipend will be
+                provided. You will be provided with an experience certificate by
+                the end of the program. If you perform up to the mark during the
+                term, the team may consider a stipend.
+              </p>
+
+              <p style="font-size: 26px">
+                <b>Duration: </b>
+              </p>
+              <p style="font-size: 26px">3 months</p>
+            </div>
+          </div>
+          <div class="row">
+            <h6>Program Coordinator</h6>
+          </div>
+
+          <div class="row">
+            <p>- Niha Zulfiker</p>
+          </div>
+
+          <!-- <div class="row">
+            <q-btn class="bg-primary text-white q-ma-md" label="register">
+            </q-btn>
+            <q-btn class="bg-primary text-white q-ma-md" label="View Schedule">
+            </q-btn>
+          </div> -->
+          <h3>Pehia DrupalCon Scholarship</h3>
+          <div class="row">
+            <div class="col-md-6 col-sm-12 q-pa-md">
+              <h4>Introduction</h4>
+              <p style="font-size: 26px">
+                DrupalCon is a mission-centric program of the Drupal Association
+                designed to bring people together to share thought leadership
+                around open source and ambitious digital experiences, provide
+                professional development opportunities to enhance your career
+                and your organization, and invigorate Drupal project momentum.
+                Pehia is one among the 28 Global partners through which
+                scholarships are provided.
+              </p>
+            </div>
+            <div class="col-md-6 col-sm-12 q-pa-md">
+              <h4>Objective</h4>
+              <p style="font-size: 26px">
+                As part of DrupalCon Globa's mission to ensure that the event
+                lives up to its global name, they are offering FIVE DrupalCon
+                Global 2020 scholarship tickets through Pehia Foundation.
+              </p>
+            </div>
+          </div>
+
+          <h4>Program Design</h4>
+
+          <div class="row">
+            <div class="col-md-6 col-sm-12 q-pa-md">
+              <p style="font-size: 26px">
+                1. Are available to take part in DrupalCon Global 2020, online,
+                July 14 - 17
+              </p>
+              <p style="font-size: 26px">
+                2. Have a demonstrated interest in one of the following - Web,
+                content management, accessibility, marketing technology, or
+                other associated technologies. Participants do not necessarily
+                need experience in Drupal.
+              </p>
+              <p style="font-size: 26px">
+                3. Would not normally be able to come to DrupalCon, due to
+                financial or travel restrictions
+              </p>
+              <p style="font-size: 26px">
+                4. Come from a group often underrepresented or underserved in
+                the open source tech communities
+              </p>
+            </div>
+            <div class="col-md-6 col-sm-12 q-pa-md">
+              <p style="font-size: 26px">
+                5. Have not previously attended DrupalCon in North America
+              </p>
+              <p style="font-size: 26px">
+                6. Are willing to take lessons learned at DrupalCon Global back
+                to their own tech communities, through such means as blog posts,
+                social media etc
+              </p>
+              <p style="font-size: 26px">
+                7. Additional Criteria : Is a Part of Pehia's online group on
+                whatsapp or have attended one of Pehia's events before
+              </p>
+            </div>
+          </div>
+          <h2>B. Opportunities from around the world</h2>
+          <p style="font-size: 26px">
+            The world is full of opportunities and finding them is a tedious
+            tasks. The well built team at Pehia takes the pain of accumulating
+            the best from around the world at one place. Pehia’s Discord server
+            is open to more inclusive goals.
+          </p>
+
+          <p style="font-size: 26px">
+            Join Pehia’s Discord server at
+            <a href="https://discord.gg/JdczjwMcSn"
+              >https://discord.gg/JdczjwMcSn</a
+            >
+            for never missing out on what the world has to offer for your
+            growth.
+          </p>
+        </div>
+      </div>
+
+      <!-- <div class="col-md-6 col-sm-12">
+        <div>
+          <q-img
+            src="~assets/Oreti.png"
+            fit="contain"
+            style="
+              max-width: 700px;
+              width: 100%;
+              max-height: 429.8px;
+              height: 100%;
+            "
+          ></q-img>
+        </div>
+      </div> -->
+    </div>
+
+    <!-- <div class="row flex flex-center" style="height: 100vh">
+      <div class="col-md-7 col-sm-12">
+        <div class="q-ma-xl">
+          <q-img
+            src="~assets/Oreti.png"
+            fit="contain"
+            style="
+              max-width: 700px;
+              width: 100%;
+              max-height: 429.8px;
+              height: 100%;
+            "
+          ></q-img>
+        </div>
+      </div>
+      <div class="col-md-5 col-sm-12">
+        <div class="q-pa-lg text-white">
+          <p style="font-size: 26px">
+            <b> Mission:</b> Pehia works to build relations among students and
+            professionals, thereby helping to bridge the technical skill gap to
+            form a space with the capacity to include everyone and anyone in the
+            field of science and technology without the bias and discrimination
+            in factors viz gender, status, financial ability,etc.
+
+            <b>Vision:</b> To serve and to be a prime ally of inclusivity and
+            bias-free society to create a world where anyone can be free to
+            learn and live up to their freedom.
+          </p>
+
+        </div>
+      </div>
+    </div> -->
+    <!-- <div class="row" style="width: 100%">
+      <h4 style="width: 100%" class="text-white text-center">Speakers</h4>
+    </div>
+    <div class="row flex flex-center">
+      <SpeakerCard></SpeakerCard>
+      <SpeakerCard></SpeakerCard>
+      <SpeakerCard></SpeakerCard>
+    </div>
+    <div class="row flex flex-center">
+      <SpeakerCard></SpeakerCard>
+      <SpeakerCard></SpeakerCard>
+      <SpeakerCard></SpeakerCard>
+    </div> -->
+
+    <!-- <div class="row flex flex-center" style="height: 100vh">
+      <div class="col-md-7 col-sm-12">
+        <div class="q-ma-xl">
+
+          <iframe
+            class="gt-sm"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3928.5332357287343!2d76.35204481465685!3d10.055321192813226!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b080c04e3627551%3A0xeaddd199d9bbde2b!2sKerala%20Technology%20Innovation%20Zone%20(KTIZ)!5e0!3m2!1sen!2sin!4v1672255654580!5m2!1sen!2sin"
+            style="
+              border-radius: 30px;
+              max-width: 700px;
+              width: 100%;
+              height: 429px;
+            "
+            allowfullscreen=""
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
+          ></iframe>
+
+          <iframe
+            class="lt-md"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3928.5332357287343!2d76.35204481465685!3d10.055321192813226!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b080c04e3627551%3A0xeaddd199d9bbde2b!2sKerala%20Technology%20Innovation%20Zone%20(KTIZ)!5e0!3m2!1sen!2sin!4v1672255654580!5m2!1sen!2sin"
+            style="
+              border-radius: 30px;
+              max-width: 700px;
+              width: 100%;
+              max-height: 429px;
+              height: 100%;
+            "
+            allowfullscreen=""
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </div>
+      </div>
+      <div class="col-md-5 col-sm-12">
+        <div class="q-pa-lg text-white">
+          <h3>See Y’all there</h3>
+          <p style="font-size: 20px">January 17th 2022</p>
+          <p style="font-size: 20px">
+            Kerala Technology Innovation Zone (KTIZ) കേരള ടെക്നോളജി ഇന്നൊവേഷൻ
+            സോൺ
+          </p>
+          <div class="row">
+            <q-btn
+              class="bg-primary text-white"
+              icon="navigation"
+              label="Navigate"
+            >
+            </q-btn>
+          </div>
+        </div>
+      </div>
+    </div> -->
+  </q-page>
+</template>
+
+<script>
+import SpeakerCard from "app/src/components/SpeakerCard.vue";
+export default {
+  components: {
+    // SpeakerCard,
+  },
+  // name: 'PageName',
+};
+</script>
+<style scoped>
+.home_bg {
+  background-color: rgba(11, 14, 41, 0.72);
+}
+</style>
