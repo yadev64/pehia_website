f911d886923151c7b7c448f1c49bd4296da4f7d4
YadevJayachandran <yadev64@gmail.com>
2023-01-06 11:20:20 +0530

    P2P Learning page created

---
+++ src/pages/P2PLearningPage.vue
@@ -0,0 +1,245 @@
+<template>
+  <q-page class="bg-secondary" padding>
+    <div class="row">
+      <div class="col-md-12 col-sm-12">
+        <div class="q-pa-lg text-white">
+          <h2>P2P Sessions</h2>
+          <div class="row">
+            <div class="col-md-6 col-sm-12 q-pa-md">
+              <h4>Introduction</h4>
+              <p style="font-size: 26px">
+                Peer learning is an educational practice in which students
+                interact with other students to learn something new. Students
+                learn a great deal by explaining their ideas to others and
+                participating in activities in which they can learn from their
+                peers.
+              </p>
+            </div>
+            <div class="col-md-6 col-sm-12 q-pa-md">
+              <h4>Objective</h4>
+              <p style="font-size: 26px">
+                Pehia Foundation launched P2P sessions aiming to improve the
+                technical skill sets of the registering members through a give
+                and take method. The program focused on gathering people with
+                the same mentality under one umbrella to bring technology to its
+                best.
+              </p>
+            </div>
+          </div>
+
+          <h4>Program Design</h4>
+
+          <div class="row">
+            <div class="col-md-6 col-sm-12 q-pa-md">
+              <p style="font-size: 26px">
+                1. <b>P2P 1.0</b> Peer to peer 1.0 focused on web development.
+                The session on HTML was handled by Ms Riya Benoy from Rajagiri
+                College of Engineering. The session was also graced by Ms
+                Keerthana K Krishnan, a software engineer from Baker Hughes, who
+                intimidated the students about the outreachy scholarship. Ms
+                Adheetha from Rajagiri college of engineering took the lead
+                thereafter and mentioned selectors.
+              </p>
+              <p>- VENUE : INQ PLARIVATTAM</p>
+              <p>- DATE : 18/02/2018 - SUNDAY</p>
+              <p>- TIME : 9.30 - 3.30</p>
+
+              <p style="font-size: 26px">
+                2. <b>P2P 1.1</b> P2P 1.1 was all about CSS. The whole session
+                was lead by Ms Adheetha. Five girls From peer to peer 1.0
+                attended the session. The session began with brushing up HTML.
+                They practised and created a page on HTML on their own. Later
+                Adheetha continued teaching CSS. After that, they were given a
+                project. The one who creates the best page will be rewarded.
+                Discussing the project and the session dispersed.
+              </p>
+              <p>- VENUE : NASSCOM WAREHOUSE</p>
+              <p>- DATE : 25/02/2018 - SUNDAY</p>
+              <p>- TIME : 9.30 - 3.30</p>
+
+              <h6>Program Coordinators</h6>
+              <p>- Ms Enfa Rose George</p>
+              <p>- Ms Sreepriya Radhakrishnan</p>
+
+              <h6>Thanks</h6>
+              <p>- Ms Riya Benoy</p>
+              <p>- Ms Keerthana K Krishnan</p>
+              <p>- Ms Adheetha</p>
+            </div>
+            <div class="col-md-6 col-sm-12 q-pa-md">
+              <p style="font-size: 26px">
+                3. <b>P2P 2.0 </b> First online p2p session for Django
+                framework. 11 participants were divided into 3 learning cohorts
+                under 3 mentors Jerine John, Melvin and Vishnu Anilkumar. The
+                learning sessions have 2 types of session namely, peer sessions
+                on weekdays and mentor sessions on weekends. By the end of the
+                learning programme, the idea was to build 3 different apps by
+                the 3 groups. Mentor sessions to teach the concepts. And peer
+                sessions were aimed at solving the tasks mentors assigned to
+                each group for building the app. Peer learning sessions started
+                with initial weeks of python sessions for beginners.
+              </p>
+              <p>- Start date: 25th April 2021</p>
+              <p>- End date: June 6th 2021</p>
+              <!-- <p>- TIME : 9.30 - 3.30</p> -->
+
+              <p style="font-size: 26px">
+                4. <b>P2P 3.0 </b> Online learning sessions on Data Analytics.
+                Starts with Basic python sessions. The session divided into 2
+                sections, one for python and followed by a Data Analytics
+                session.
+              </p>
+              <p>- Python mentors : Jaimy Simon, Kiran Johns, Kiran P K</p>
+              <!-- <p>- Data Analytics mentor : TBA</p> -->
+              <p>- No of attendees : 27</p>
+            </div>
+          </div>
+
+          <!-- <div class="row">
+            <q-btn class="bg-primary text-white q-ma-md" label="register">
+            </q-btn>
+            <q-btn class="bg-primary text-white q-ma-md" label="View Schedule">
+            </q-btn>
+          </div> -->
+
+          <div class="row">
+            <div class="col-md-6 col-sm-12 q-pa-md">
+              <h6>Program Coordinators</h6>
+              <p>- Anusha M R</p>
+              <p>- Ruhi Thomas</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- <div class="col-md-6 col-sm-12">
+        <div>
+          <q-img
+            src="~assets/Oreti.png"
+            fit="contain"
+            style="
+              max-width: 700px;
+              width: 100%;
+              max-height: 429.8px;
+              height: 100%;
+            "
+          ></q-img>
+        </div>
+      </div> -->
+    </div>
+
+    <!-- <div class="row flex flex-center" style="height: 100vh">
+      <div class="col-md-7 col-sm-12">
+        <div class="q-ma-xl">
+          <q-img
+            src="~assets/Oreti.png"
+            fit="contain"
+            style="
+              max-width: 700px;
+              width: 100%;
+              max-height: 429.8px;
+              height: 100%;
+            "
+          ></q-img>
+        </div>
+      </div>
+      <div class="col-md-5 col-sm-12">
+        <div class="q-pa-lg text-white">
+          <p style="font-size: 26px">
+            <b> Mission:</b> Pehia works to build relations among students and
+            professionals, thereby helping to bridge the technical skill gap to
+            form a space with the capacity to include everyone and anyone in the
+            field of science and technology without the bias and discrimination
+            in factors viz gender, status, financial ability,etc.
+
+            <b>Vision:</b> To serve and to be a prime ally of inclusivity and
+            bias-free society to create a world where anyone can be free to
+            learn and live up to their freedom.
+          </p>
+
+        </div>
+      </div>
+    </div> -->
+    <!-- <div class="row" style="width: 100%">
+      <h4 style="width: 100%" class="text-white text-center">Speakers</h4>
+    </div>
+    <div class="row flex flex-center">
+      <SpeakerCard></SpeakerCard>
+      <SpeakerCard></SpeakerCard>
+      <SpeakerCard></SpeakerCard>
+    </div>
+    <div class="row flex flex-center">
+      <SpeakerCard></SpeakerCard>
+      <SpeakerCard></SpeakerCard>
+      <SpeakerCard></SpeakerCard>
+    </div> -->
+
+    <!-- <div class="row flex flex-center" style="height: 100vh">
+      <div class="col-md-7 col-sm-12">
+        <div class="q-ma-xl">
+
+          <iframe
+            class="gt-sm"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3928.5332357287343!2d76.35204481465685!3d10.055321192813226!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b080c04e3627551%3A0xeaddd199d9bbde2b!2sKerala%20Technology%20Innovation%20Zone%20(KTIZ)!5e0!3m2!1sen!2sin!4v1672255654580!5m2!1sen!2sin"
+            style="
+              border-radius: 30px;
+              max-width: 700px;
+              width: 100%;
+              height: 429px;
+            "
+            allowfullscreen=""
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
+          ></iframe>
+
+          <iframe
+            class="lt-md"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3928.5332357287343!2d76.35204481465685!3d10.055321192813226!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b080c04e3627551%3A0xeaddd199d9bbde2b!2sKerala%20Technology%20Innovation%20Zone%20(KTIZ)!5e0!3m2!1sen!2sin!4v1672255654580!5m2!1sen!2sin"
+            style="
+              border-radius: 30px;
+              max-width: 700px;
+              width: 100%;
+              max-height: 429px;
+              height: 100%;
+            "
+            allowfullscreen=""
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </div>
+      </div>
+      <div class="col-md-5 col-sm-12">
+        <div class="q-pa-lg text-white">
+          <h3>See Y’all there</h3>
+          <p style="font-size: 20px">January 17th 2022</p>
+          <p style="font-size: 20px">
+            Kerala Technology Innovation Zone (KTIZ) കേരള ടെക്നോളജി ഇന്നൊവേഷൻ
+            സോൺ
+          </p>
+          <div class="row">
+            <q-btn
+              class="bg-primary text-white"
+              icon="navigation"
+              label="Navigate"
+            >
+            </q-btn>
+          </div>
+        </div>
+      </div>
+    </div> -->
+  </q-page>
+</template>
+
+<script>
+import SpeakerCard from "app/src/components/SpeakerCard.vue";
+export default {
+  components: {
+    // SpeakerCard,
+  },
+  // name: 'PageName',
+};
+</script>
+<style scoped>
+.home_bg {
+  background-color: rgba(11, 14, 41, 0.72);
+}
+</style>
